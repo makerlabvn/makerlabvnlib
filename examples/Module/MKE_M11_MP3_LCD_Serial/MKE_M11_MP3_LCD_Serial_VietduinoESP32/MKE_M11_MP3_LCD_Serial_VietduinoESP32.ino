9544e49c46a4dda19dc4de6c1e3fa4bd118c2e25
giaan1901 <90020583+giaan1901@users.noreply.github.com>
2025-06-23 12:19:35 +0700

    Update MKE_M11_MP3_LCD_Serial_VietduinoESP32.ino

--- examples/Module/MKE_M11_MP3_LCD_Serial/MKE_M11_MP3_LCD_Serial_VietduinoESP32/MKE_M11_MP3_LCD_Serial_VietduinoESP32.ino
+++ examples/Module/MKE_M11_MP3_LCD_Serial/MKE_M11_MP3_LCD_Serial_VietduinoESP32/MKE_M11_MP3_LCD_Serial_VietduinoESP32.ino
@@ -22,51 +22,61 @@ void setup()
   lcd.init();
   lcd.backlight();
 
-  // Khởi động kết nối Serial UART ở tốc độ 115200 để truyền dữ liệu lên máy tính
-  // Start the Serial UART connection at 115200 to transfer data to the computer
+  // Khởi động kết nối Serial UART ở tốc độ 115200 để truyền dữ liệu lên máy tính.
+  // Start the Serial UART connection at 115200 to transfer data to the computer.
   Serial.begin(115200);
 
-  // Khởi tạo module với Serial2
-  // Initialize module with Serial2
-  Serial2.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
+  // Khởi tạo module
+  // init module
+  mySoftwareSerial.begin(9600);
   delay(200);
-  myDFPlayer.begin(Serial2, true, true);
+  myDFPlayer.begin(mySoftwareSerial, true,true);
   delay(100);
   
+  // Cài đặt giao tiếp serial mỗi 500ms
+  // Set serial communictaion time out 500ms
   myDFPlayer.setTimeOut(500);
-  myDFPlayer.volume(30); // Volume range: 0-30
+
+  // Cài đặt âm lượng (3~30).
+  // Set volume value (0~30).
+  myDFPlayer.volume(30);
+  
 }
 
 void loop()
 {
+  // Phát file mp3 ở đường dẫn SD:/MP3/0001.mp3
+  // Play specific mp3 in SD:/MP3/0001.mp3
   myDFPlayer.playMp3Folder(1);
 
-  // Hiển thị giá trị của module lên máy tính
-  // Show the module value on Arduino Serial Monitor
+  // Hiển thị giá trị của module lên máy tính.
+  // Show the module value on Arduno Serial Monitor
   Serial.println("Play file 1");
 
-  // Gửi giá trị module lên LCD
-  // Show the module value on LCD
-  lcd.setCursor(0, 0);
-  lcd.print("Play file 1    "); // Thêm khoảng trắng để xóa văn bản cũ
+  //Gửi giá trị module lên LCD
+  //Show the module value on LCD
+  lcd.setCursor(0,0);
+  lcd.print("Play file 1");
 
   // Chờ 2000ms
-  // Wait 2000ms
+  // Wait 2000ms  
   delay(2000);
 
+  // Dừng file đang phát
+  // Stop the current file
   myDFPlayer.stop();
 
-  // Hiển thị giá trị của module lên máy tính
-  // Show the module value on Arduino Serial Monitor
+  // Hiển thị giá trị của module lên máy tính.
+  // Show the module value on Arduno Serial Monitor
   Serial.println("Stop file 1");
 
-  // Gửi giá trị module lên LCD
-  // Show the module value on LCD
+  //Gửi giá trị module lên LCD
+  //Show the module value on LCD
   lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Stop file 1    "); // Thêm khoảng trắng để xóa văn bản cũ
+  lcd.setCursor(0,0);
+  lcd.print("Stop file 1");
 
   // Chờ 2000ms
-  // Wait 2000ms
+  // Wait 2000ms  
   delay(2000);
 }
